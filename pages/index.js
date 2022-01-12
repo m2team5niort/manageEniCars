83bf83b0f9c85dbe2f104d926a17f276b270df41
Antony <cochetantony@gmail.com>
2022-01-12 15:15:36 +0100

    test without import component

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
-import Navbar from "../components/homepage/Navbar"
-import Header from "../components/homepage/Header"
-import Features from "../components/homepage/Features"
-
 export default function Home() {
 
   return (
 
     <>
-      <Navbar header />
-      <Features />
+      ok
     </>
 
   )
