94c37c729759c2a6c6883c207759def839723ed1
brandon.nalin <brandon.nalin@nxo.eu>
2022-05-04 13:33:54 +0200

    Start Key

---
+++ pages/dashboard/keys.js
@@ -0,0 +1,35 @@
+import { withSSRContext } from 'aws-amplify'
+import React from 'react'
+import Model from '../../Components/Dashboard/Model'
+import Sidebar from '../../Components/Dashboard/Sidebar'
+import Navbar from '../../Components/Dashboard/Navbar'
+
+function keys({ username }) {
+
+  return (
+    <div className={`container-dashboard mx-auto bg-gray-900`}>
+        <Navbar />
+        <Sidebar />
+        <Model username={username}/>
+    </div>
+  )
+}
+
+export async function getServerSideProps({ req, res }) {
+    const { Auth } = withSSRContext({ req })
+    try {
+      const user = await Auth.currentAuthenticatedUser()
+      return {
+        props: {
+          username: user.username,
+        }
+      }
+    } catch (err) {
+      res.writeHead(302, { Location: '/signup' })
+      res.end()
+    }
+    return {props: {}}
+  }
+  
+
+export default keys
