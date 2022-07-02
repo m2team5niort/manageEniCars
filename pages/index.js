b7d48fa08f1210b7fce878a170619f7b23fc22bf
brandon.nalin <brandon.nalin@nxo.eu>
2022-07-02 17:41:27 +0200

    Modif Front Home Page

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,17 @@ export default function Home() {
 
   return (
 
-    <>
+    <div className="bg-gray-100 text-gray-700 bg-white antialiased">
 
       <div className="bg-white min-h-screen">
         <nav className="flex items-center justify-between flex-wrap p-6 space-between w-full bg-indigo-800">
 
-          <div className="flex items-center flex-shrink-0 text-dark md:text-right">
-            <h1 className="text-2xl text-white font-semibold"> ManageCars </h1>
-          </div>
+        {/* Logo Navbar */}
+        <div className="flex items-center flex-shrink-0 text-dark md:text-right mr-6">
+          {/* <img src="../assets/images/Compressed_Images/Logo_Compressed.png" style={{width: '100%', height: '50px'}} loading="lazy"/> */}
+          <h1 className="text-gray-600"> ManageCars </h1>
+          
+        </div>
 
           <div className="block lg:hidden">
             <button id="nav-toggle" className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-800 hover:border-teal-500 appearance-none focus:outline-none">
@@ -25,50 +28,72 @@ export default function Home() {
             </button>
           </div>
 
-          <div id="nav-content">
-            <div className="text-sm lg:flex-grow">
-              <Link href="/">
-                <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 mr-4">
-                  <button className="text-white font-semibold hover:text-teal-200"> Accueil </button>
-                </a>
-              </Link>
-              <Link href="/signup">
-                <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 mr-4">
-                  <button className="text-white font-semibold hover:text-teal-200"> Connexion </button>
-                </a>
-              </Link>
-            </div>
-          </div>
-        </nav>
-      </div>
+        {/* Menu Navbar (Links) */}
+        <div id="nav-content" className="">
+          <div className="text-sm lg:flex-grow ">
 
-      <div className="bg-gradient-to-br from-gray-900 to-indigo-900 min-h-screen flex items-center">
 
-        <div className="px-12">
-
-          <div className="flex justify-center items-center relative space-x-8">
-
-            <img onClick={() => console.log('click')} className="opacity-50 shadow-2xl object-cover h-60 w-96 rounded-md hover:opacity-100 transition ease-in duration-300 cursor-pointer" src="../assets/images/homepage/b6.jpg" />
-
-            <div className="flex flex-row relative shadow-2xl homepage-car rounded-3xl">
-              <h1 className="text-9xl text-white font-semibold -top-20 -left-24 absolute"> Mégane 5 </h1>
-              <div className="flex flex-row absolute bottom-6 left-8 space-x-8 justify-center">
-                <div className="bg-indigo-800 shadow-2xl hover:bg-indigo-700 transition ease-in duration-300">
-                  <button className="text-white rounded py-2 px-6">Réserver ce véhicule</button>
-                </div>
-                <div className="bg-indigo-800 shadow-2xl hover:bg-indigo-700 transition ease-in duration-300">
-                  <button className="text-white rounded py-2 px-6">Plus d'informations</button>
-                </div>
-              </div>
-            </div>
-
-            <img onClick={() => console.log('click')} className="opacity-50 shadow-2xl object-cover h-60 w-96 rounded-md hover:opacity-100 transition ease-in duration-300 cursor-pointer" src="../assets/images/homepage/b7.jpg" />
-
+            <Link href="/">
+              <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-gray-600 hover:text-teal-800 mr-4">
+                <button className="text-gray-600 font-semibold"> Accueil </button>
+              </a>
+            </Link>
+            <Link href="/signup">
+              <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-gray-600 hover:text-teal-800 mr-4">
+                <button className="text-gray-600 font-semibold"> Connexion </button>
+              </a>
+            </Link>
           </div>
         </div>
+      </nav>
 
+    {/* Header */}
+    <div className="header ">
+
+      {/* NavBar Height */}
+      <div className="row h-20 "></div>
+
+      {/* Title Container */}
+      <div className="row ">
+        
+        {/* Title */}
+        <div className="w-1/4 h-2/4 ml-96 mt-24">
+          <h1 className="text-9xl "> Mégane 5 </h1>
+        </div>
       </div>
-      <footer className="bg-white h-12 text-center pt-12" style={{ display: 'flex' }}>
+
+      {/* Image Container */}
+      <div className="row ">
+        
+        {/* Title */}
+        <div className="mt-24 flex justify-center relative">
+          
+          <img className="w-8/12 ml-72 shadow-2xl" src="../assets/images/Background/b5.jpeg"/>
+
+          <div className="flex flex-col absolute -bottom-12 right-48 space-y-8">
+            <div className="w-48 h-48 bg-gray-800 shadow-2xl">
+              <img className="object-cover h-full w-full" src="../assets/images/Background/b5.jpeg"/>
+            </div>
+            <div className="w-48 h-48 bg-gray-800 shadow-2xl">
+              <img className="object-cover h-full w-full" src="../assets/images/Background/b5.jpeg"/>
+            </div>
+          </div>
+          
+          <div className="flex flex-row absolute bottom-8 space-x-8 justify-center mr-12">
+            <div className="bg-gray-300 shadow-2xl">
+              <button className=" text-gray-800 rounded py-4 px-8 font-bold"> Réserver ce véhicule </button>
+            </div>
+            <div className="bg-gray-300 shadow-2xl">
+              <button className=" text-gray-800 rounded py-4 px-8 font-bold"> Plus d'informations </button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+      {/* <footer className="bg-white h-12 text-center pt-12" style={{ display: 'flex' }}>
+
         <ul className="text-gray-600 flex justify-center w-full pt-2  ">
           <li className="pr-8" > ENI ManageCars © 2022 </li>
           <li className="pr-8" > Mentions légales </li>
@@ -76,7 +101,13 @@ export default function Home() {
           <li className="pr-8"> Plan du site </li>
           <li className=""> Actualité </li>
         </ul>
-      </footer>
-    </>
+      </footer> */}
+
+
+
+      <Script src="../assets/js/script.js" />
+
+    </div>
+
   )
 }
